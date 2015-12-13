8207d74a125fd466bede4e3f9acf6de982f9a30e
almyu <hire@indeed.li>
2015-12-13 16:00:50 +0700

    Add shaker

---
+++ Assets/LD34/Scripts/Scenery/Shaker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace LD34 {
+
+    public class Shaker : MonoBehaviour {
+
+        public float maxDist = 0.05f;
+
+        private Vector3 lastOffset;
+
+        private void OnEnable() {
+            lastOffset = Vector3.zero;
+        }
+
+        private void Update() {
+            transform.position -= lastOffset;
+            lastOffset = Random.insideUnitCircle * maxDist;
+            transform.position += lastOffset;
+        }
+
+        private void OnDisable() {
+            transform.position -= lastOffset;
+        }
+    }
+}
